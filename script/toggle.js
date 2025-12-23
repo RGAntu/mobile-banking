5bad67df56d80987d0c879459a63bf961f539ee0
RGAntu <rgantu887@gmail.com>
2025-12-23 23:31:25 +0600

    added toggle in add-money & cash-out-box

--- script/toggle.js
+++ script/toggle.js
@@ -1,10 +1,23 @@
-document.getElementById('bd-btn').addEventListener("click", function(){
-   document.getElementById("bd").style.display = "block";
-   document.getElementById("usa").style.display = "none"
+// document.getElementById('bd-btn').addEventListener("click", function(){
+//    document.getElementById("bd").style.display = "block";
+//    document.getElementById("usa").style.display = "none"
+// })
+
+
+// document.getElementById("usa-btn").addEventListener("click", function (){
+//     document.getElementById("bd").style.display = "none";
+//    document.getElementById("usa").style.display = "block"
+// })
+
+
+// initially add money hidden 
+document.getElementById('AddMoney').style.display = "none"
+
+document.getElementById('add-money-box').addEventListener("click", function(){
+    document.getElementById('cashOut').style.display = "none"
+    document.getElementById('AddMoney').style.display = "block"
 })
-
-
-document.getElementById("usa-btn").addEventListener("click", function (){
-    document.getElementById("bd").style.display = "none";
-   document.getElementById("usa").style.display = "block"
+document.getElementById('cash-out-box').addEventListener("click", function(){
+    document.getElementById('AddMoney').style.display = "none"
+    document.getElementById('cashOut').style.display = "block"
 })
